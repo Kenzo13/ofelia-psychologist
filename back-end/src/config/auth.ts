a8df1d89f6967b9ff54f5dcd8a86bf600e10a715
kenzo13 <raphaelkenzo2586@gmail.com>
2021-02-25 17:06:28 -0300

    Creating middlewares to authenticate the user

---
+++ back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '898d8b8615db1dbd8dcb032fd98fd23b',
+    expiresIn: '1d',
+  },
+};
--- back-end/src/services/AuthenticateUserService.ts
+++ back-end/src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '898d8b8615db1dbd8dcb032fd98fd23b', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {

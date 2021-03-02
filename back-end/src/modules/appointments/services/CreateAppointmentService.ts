7f668336d34e670da2a2378855263e4b89e409d9
kenzo13 <raphaelkenzo2586@gmail.com>
2021-03-02 11:05:20 -0300

    changing the project to a DDD design partners

--- back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,11 +1,10 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentRepository from '../repositories/AppointmentRepository';
-
-import AppError from '../errors/AppError';
-
-import Appointment from '../models/Appointment';
 
 interface Request {
   provider_id: string;

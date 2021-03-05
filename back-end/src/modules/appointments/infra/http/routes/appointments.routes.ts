07f173e54422be7cdf84cdebe856e70134b4fb59
kenzo13 <raphaelkenzo2586@gmail.com>
2021-03-05 17:28:02 -0300

    Creating controllers

--- back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,10 @@
 import Router from 'express';
-import { parseISO } from 'date-fns';
-
-import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentRepository';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentRepository();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -17,21 +14,6 @@ appointmentsRouter.use(ensureAuthenticated);
 //   return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = new CreateAppointmentService(
-    appointmentsRepository,
-  );
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;

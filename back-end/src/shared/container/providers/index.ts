15a7ad406d76bfdc8c713172588a3a5313a1f856
kenzo13 <raphaelkenzo2586@gmail.com>
2021-03-10 16:24:42 -0300

    Creating a implementation of recover mail password using TDD

---
+++ back-end/src/shared/container/providers/index.ts
@@ -0,0 +1,11 @@
+import { container } from 'tsyringe';
+
+import IStorageProvider from './StorageProviders/models/IStorageProvider';
+import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
+
+// import IMailProvider from './MailProvider/models/IMailProvider';
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  DiskStorageProvider,
+);

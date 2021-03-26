6bf1b2458aea0186b290e0bac589294cf4a2893c
kenzo13 <raphaelkenzo2586@gmail.com>
2021-03-26 15:15:18 -0300

    Listing days and hours

--- back-end/src/modules/appointments/services/ListProvidersService.spec.ts
+++ back-end/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -4,7 +4,7 @@ import ListProvidersService from './ListProvidersService';
 let fakeUsersRepository: FakeUsersRepository;
 let listProviders: ListProvidersService;
 
-describe('UpdateProfile', () => {
+describe('ListProviders', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
 
